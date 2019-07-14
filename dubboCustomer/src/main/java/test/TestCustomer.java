10ce3fd70afacae974406c74f6702609cbdb9891
royzhuo <731924600@qq.com>
2019-07-14 13:38:29 +0800

    启动时检查

--- dubboCustomer/src/main/java/test/TestCustomer.java
+++ dubboCustomer/src/main/java/test/TestCustomer.java
@@ -11,11 +11,14 @@ public class TestCustomer {
 
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext ioc=new ClassPathXmlApplicationContext("cutomer.xml");
+        System.out.println("0k ok ok");
         OrderService orderService= (OrderService) ioc.getBean(OrderService.class);
         List<UserAddress> userAddresses=orderService.getUserAddress();
         for (UserAddress userAddress:userAddresses){
             System.out.println(userAddress);
         }
+
         System.in.read();
+
     }
 }
