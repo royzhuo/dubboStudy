1ab180417c6047d57aa9b4276eeb7a35717a5322
royzhuo <731924600@qq.com>
2019-07-14 14:24:41 +0800

    dubbo 时间超时设置

--- dubboProvide/src/main/java/com/dubbo/provide/serviceImpl/AddressServiceImpl.java
+++ dubboProvide/src/main/java/com/dubbo/provide/serviceImpl/AddressServiceImpl.java
@@ -15,6 +15,11 @@ public class AddressServiceImpl implements AddressService {
 
     @Override
     public List<UserAddress> getUserAddress(String userId) {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         UserAddress userAddress1=new UserAddress("1","10010","roy","中国福建省厦门市");
         UserAddress userAddress2=new UserAddress("2","10011","jessica","中国福建省厦门市思明区");
         return Arrays.asList(userAddress1,userAddress2);
